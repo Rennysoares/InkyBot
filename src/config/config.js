9e0e85d10b8cd9af7f6a0ada0c6b61767d159bef
rennysoares <117554741+Rennysoares@users.noreply.github.com>
2024-05-05 23:29:09 -0400

    InkyBot 0.6.1

--- src/config/config.js
+++
@@ -1,9 +0,0 @@
-const botSettings = {
-    number: 'YOUR_NUMBER_HERE',
-    prefixes: ['.', '/', '!', '#', '+'],
-    isConsoleLog: true,
-    geminiApiKey: "API_KEY_HERE"
-}
-module.exports = {
-    botSettings
-}
